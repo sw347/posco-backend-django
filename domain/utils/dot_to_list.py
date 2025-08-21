332467ac2f9c8c36ac1de82dd8ccd19dadf86f5c
jwleetop5 <jwleetop5@naver.com>
2025-08-21 13:48:46 +0900

    add braille_to_list

---
+++ domain/utils/dot_to_list.py
@@ -0,0 +1,33 @@
+import itertools
+
+def braille_to_list(braille_string):
+  result_list = []
+
+  braille_base_code = 0x2800
+
+  for char in braille_string:
+
+    char_code = ord(char)
+
+    pattern_value = char_code - braille_base_code
+
+    dot_pattern = [0] * 6
+
+    if pattern_value & 1:
+      dot_pattern[0] = 1
+    if pattern_value & 2:
+      dot_pattern[1] = 1
+    if pattern_value & 4:
+      dot_pattern[2] = 1
+    if pattern_value & 8:
+      dot_pattern[3] = 1
+    if pattern_value & 16:
+      dot_pattern[4] = 1
+    if pattern_value & 32:
+      dot_pattern[5] = 1
+
+    result_list.append(dot_pattern)
+
+  flat_list = list(itertools.chain.from_iterable(result_list))
+  
+  return flat_list
