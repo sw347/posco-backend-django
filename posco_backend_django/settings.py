64c304fd37085940e8bf5cbbb1986bc24ae3be32
sw347 <sdh230310@sdh.hs.kr>
2025-09-16 20:02:21 +0200

    chore: ALLOWED_HOSTS 추가

--- posco_backend_django/settings.py
+++ posco_backend_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%$3@zdu*)mr+0uxl3rh-&gn0wxsct_89hd-n_t^6znce!ke52i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['222.110.147.50']
 
 
 # Application definition
